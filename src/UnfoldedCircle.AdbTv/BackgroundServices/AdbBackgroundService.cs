e67986a227603911c93afc2d87d958c541bbfcf9
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2025-08-25 13:13:00 +0200

    Ensure that adb server keeps running

--- src/UnfoldedCircle.AdbTv/BackgroundServices/AdbBackgroundService.cs
+++ src/UnfoldedCircle.AdbTv/BackgroundServices/AdbBackgroundService.cs
@@ -1,44 +1,38 @@
-using System.Diagnostics;
+using AdvancedSharpAdbClient;
 
 namespace UnfoldedCircle.AdbTv.BackgroundServices;
 
 public sealed class AdbBackgroundService : IHostedService
 {
+    private readonly CancellationTokenSource _keepAliveCancellationTokenSource = new();
+
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        using var adbProcess = new Process();
-        adbProcess.StartInfo = new ProcessStartInfo
-        {
-            FileName = "adb",
-            Arguments = "start-server",
-            UseShellExecute = false,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            CreateNoWindow = true
-        };
-        adbProcess.Start();
-
         using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
         using var linkedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, cancellationTokenSource.Token);
-        await adbProcess.WaitForExitAsync(linkedCancellationToken.Token);
+        await StartOrStop(true, linkedCancellationToken.Token);
+
+        _ = Task.Factory.StartNew(() => _ = KeepAdbServerRunning(), TaskCreationOptions.LongRunning);
+    }
+
+    private async Task KeepAdbServerRunning()
+    {
+        using var periodicTimer = new PeriodicTimer(TimeSpan.FromMinutes(5));
+        while (!_keepAliveCancellationTokenSource.IsCancellationRequested && await periodicTimer.WaitForNextTickAsync(_keepAliveCancellationTokenSource.Token))
+            await StartOrStop(true, _keepAliveCancellationTokenSource.Token);
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
-        using var adbProcess = new Process();
-        adbProcess.StartInfo = new ProcessStartInfo
-        {
-            FileName = "adb",
-            Arguments = "kill-server",
-            UseShellExecute = false,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true,
-            CreateNoWindow = true
-        };
-        adbProcess.Start();
-
         using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
         using var linkedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, cancellationTokenSource.Token);
-        await adbProcess.WaitForExitAsync(linkedCancellationToken.Token);
+        await StartOrStop(false, linkedCancellationToken.Token);
+    }
+
+    private static Task StartOrStop(bool start, CancellationToken cancellationToken)
+    {
+        return start
+            ? AdbServer.Instance.StartServerAsync("adb",false, cancellationToken)
+            : AdbServer.Instance.StopServerAsync(cancellationToken);
     }
 }
