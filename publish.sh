8c3f7256eb794efa36f553668a2fb2649355601f
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-05-14 18:39:45 +0200

    Only one file

--- publish.sh
+++ publish.sh
@@ -33,7 +33,7 @@ jq --arg version "$unprefixed_version" --arg date "$date" \
 # Copy driver.json to the root of the driver directory
 cp ./driver.json ./driverdir/
 
-# Copy appsettings*.json to the config directory
+# Copy appsettings.json to the config directory
 cp ./appsettings.json ./driverdir/config/
 
 # Copy driver (file) and *.pdb files from the publish directory to the bin directory in the driver directory
